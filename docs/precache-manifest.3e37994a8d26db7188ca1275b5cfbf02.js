f0c41ce989eef552f28799426b46cbcc9ae98949
yukihiroK <yukihiroK@users.noreply.github.com>
2020-07-03 03:22:54 +0900

    縦組み修正

---
+++ docs/precache-manifest.3e37994a8d26db7188ca1275b5cfbf02.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4398a5a041084513b4166f8f4836c2f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a880efa2841256ab4102",
+    "url": "./static/css/main.4bbd2941.chunk.css"
+  },
+  {
+    "revision": "c8c1617c259d97445c58",
+    "url": "./static/js/2.b3ec8537.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.b3ec8537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a880efa2841256ab4102",
+    "url": "./static/js/main.f3633825.chunk.js"
+  },
+  {
+    "revision": "75b4f862258e49bcebaf",
+    "url": "./static/js/runtime-main.a0fe5bbb.js"
+  },
+  {
+    "revision": "8cb46ee7a1798b06c4ab337561f81923",
+    "url": "./static/media/NotoSerifJP-Bold-Vertical.8cb46ee7.otf"
+  },
+  {
+    "revision": "95a71858588575d4c784edef475d8844",
+    "url": "./static/media/NotoSerifJP-Bold-Vertical.95a71858.woff2"
+  },
+  {
+    "revision": "b2b80596763be713404434d6f16443a7",
+    "url": "./static/media/NotoSerifJP-Bold-Vertical.b2b80596.woff"
+  },
+  {
+    "revision": "8c6b1df43cbf08debb503892adca2a0f",
+    "url": "./static/media/NotoSerifJP-Medium-Vertical.8c6b1df4.woff2"
+  },
+  {
+    "revision": "d578d9497ef2120a1bcec1dac2e0da71",
+    "url": "./static/media/NotoSerifJP-Medium-Vertical.d578d949.woff"
+  },
+  {
+    "revision": "fc084e09e4710fc09cb5d5d95a241a00",
+    "url": "./static/media/NotoSerifJP-Medium-Vertical.fc084e09.otf"
+  },
+  {
+    "revision": "8597613cf819df5673df1228261d8687",
+    "url": "./static/media/title.8597613c.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2d7da54aa2d48cffee6a81bd1c7755a5.js"
+  "./precache-manifest.3e37994a8d26db7188ca1275b5cfbf02.js"
 );
 
 self.addEventListener('message', (event) => {
